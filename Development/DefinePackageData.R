9e3b4b0d27cb8419ec6506976eadb88357c9785c
BastianReiter <150595257+BastianReiter@users.noreply.github.com>
2026-09-19 13:34:42 +0200

    Change mode

--- Development/DefinePackageData.R
+++ Development/DefinePackageData.R
@@ -85,7 +85,7 @@ Set.PrivacyProfiles <- list("Development" = MakePrivacyProfile(NThreshold = NA,
 # Save data in .rda-file and make it part of package
 use_data(Set.PrivacyProfiles, overwrite = TRUE)
 
-Set.PrivacyProfile.Chosen <- "Loose"
+Set.PrivacyProfile.Chosen <- "Development"
 use_data(Set.PrivacyProfile.Chosen, overwrite = TRUE)
 
 
